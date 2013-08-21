e8e80470ac9af40bc666b9761bd5d89feb6302f8
nasta_t <nastia.tikk@gmail.com>
2013-08-20 18:56:53 -0700

    2 file added
    bla blabla

---
+++ _header.php
@@ -0,0 +1,26 @@
+<div id="header">
+				
+	<a id="logo" href="http://sypestudios.com/" title="SYPE STUDIOS Home"></a>
+		
+		
+		
+		
+		<div id="language">
+			<a title="English"><img src="/res/images/lang/prague_recording_studio_us.jpg" /></a>
+			<a title="English"><img src="/res/images/lang/prague_recording_studio_en.jpg" /></a>
+			<a href="/cz/praha-nahravaci-studio.php" title="Czech"><img src="/res/images/lang/prague_recording_studio_cze.jpg" /></a>
+		</div>				
+		
+		<div id="header_description">
+			<h3>
+			<span>|</span> Prague audio recording studio <span>|</span></h3>
+			
+			<h3>
+			<span>|</span> affordable professional recording <span>|</span> young innovative team <span>|</span> individual creative approach <span>|</span></h3>
+			
+			<h3>
+			<span>| </span> music mixing and mastering <span>|</span> music production for games, web, film and advertisement <span>|</span></h3>
+		
+		</div>
+
+</div>
